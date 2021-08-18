23b0099c4a85472082afd9ce426f974156f662c3
min11benja <min11benja@hotmail.com>
2021-08-18 18:20:35 -0500

    Initial commit with scafolding MVC 5 template

---
+++ 5StarWifi/5StarWifi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_5StarWifi.Startup))]
+namespace _5StarWifi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

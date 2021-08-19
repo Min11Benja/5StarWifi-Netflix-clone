cd8a86a1d40ab6556066da4cc401a46c69335440
min11benja <min11benja@hotmail.com>
2021-08-19 14:08:46 -0500

    Added IsSubscribedToCustomer scafolded code with Entity Framework

---
+++ 5StarWifi/5StarWifi/Migrations/202108191906558_AddisSubscribedToCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace _5StarWifi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddisSubscribedToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "isSubscribedToCustomer", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "isSubscribedToCustomer");
+        }
+    }
+}
--- 5StarWifi/5StarWifi/Models/Customer.cs
+++ 5StarWifi/5StarWifi/Models/Customer.cs
@@ -11,6 +11,7 @@ namespace _5StarWifi.Models
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }        
+        public string Name { get; set; }
+        public bool isSubscribedToCustomer { get; set; }
     }
 }

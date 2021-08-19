c35738b51f5a6f2c2069585b67483290124cb7e7
min11benja <min11benja@hotmail.com>
2021-08-19 18:38:13 -0500

    Extracted the Nav and Footer in Shared into partial views and added the customer table code into its view

--- 5StarWifi/5StarWifi/App_Start/BundleConfig.cs
+++ 5StarWifi/5StarWifi/App_Start/BundleConfig.cs
@@ -20,7 +20,9 @@ namespace _5StarWifi
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/plugins.bundle.js",
+                      "~/Scripts/scripts.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(                      
                       "~/Content/metronic-style.css",
--- 5StarWifi/5StarWifi/Controllers/CustomersController.cs
+++ 5StarWifi/5StarWifi/Controllers/CustomersController.cs
@@ -12,7 +12,22 @@ namespace _5StarWifi.Controllers
         // GET: Customers
         public ActionResult Index()
         {
+            //check if user type is admin or manager if not redirect to login page show does not have permision error
+            //return View();
+            return Redirect("~/Customers/CustomersTable");
+        }
+        // GET: Customers/CustomersTable
+        public ViewResult CustomersTable() {
             return View();
+        }
+       
+        private IEnumerable<Customer> GetCustomers() {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John1"},
+                new Customer { Id = 2, Name = "Pablo2"},
+                new Customer { Id = 3, Name = "Teresa3"}
+            };
         }
     }
 }

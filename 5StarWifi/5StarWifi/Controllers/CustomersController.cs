8f3938fb198e80b052e2ac15fb446ea87dab9d4c
min11benja <min11benja@hotmail.com>
2021-08-22 17:19:10 -0500

    Edited the Add and Update form and controllers

--- 5StarWifi/5StarWifi/Controllers/CustomersController.cs
+++ 5StarWifi/5StarWifi/Controllers/CustomersController.cs
@@ -68,31 +68,62 @@ namespace _5StarWifi.Controllers
                 MembershipTypes = membershipTypes
             };
             return View(viewModel);            
-        }
-        //Make sure this can only be called through an Http Post and not an Http Get
-        [HttpPost]
-        public ActionResult Create(Customer customer)
-        {
-            //MVC framework binds this viewModel to the submited data
-
-            //To add the parameters into our Customers DB table we need to add it to our _context 
-            _context.Customers.Add(customer);
-            //this wont add it to the DB just yet it just gets stored in memory to persist the changes we need to call this other method
-            _context.SaveChanges();
-            return Redirect("~/Customers/CustomersTable");
-        }
+        }       
         // Customers/Edit/2
-        public ActionResult Edit(int id) {
+        public ActionResult Edit(int id)
+        {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var membershipType = _context.MembershipTypes.SingleOrDefault(m => m.Id == customer.MembershipTypeId);
+            var membershipTypesList = _context.MembershipTypes.ToList();
+
+            var viewModel = new UpdateCustomerViewModel
+            {
+                Customer = customer,
+                MembershipType = membershipType,
+                MembershipTypesList = membershipTypesList
+            };
+            //if the customers id exist we return the view else show error message            
             if (customer == null)
             {
+                //standard 404 error
                 return HttpNotFound();
             }
             else
             {
-                return View(customer);
-            }            
+                return View(viewModel);
+            }
         }
-       
+        //Make sure this can only be called through an Http Post and not an Http Get
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            //MVC framework binds this viewModel to the submited data
+            if (customer.Id == 0)
+            {
+                //its a new customer lets add it
+                //To add the parameters into our Customers DB table we need to add it to our _context 
+                _context.Customers.Add(customer);                
+            }
+            else {
+                //its an existing customer we need to update the info
+                //for this we need to get it fomr the Database first
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                //update its property based on the forms values
+                customerInDb.Name = customer.Name;
+                customerInDb.Email = customer.Email;
+                customerInDb.Password = customer.Password;
+                customerInDb.Adress = customer.Adress;
+                customerInDb.Phone = customer.Phone;
+                customerInDb.isSubscribedToCustomer = customer.isSubscribedToCustomer;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;                
+                customerInDb.StartDate = customer.StartDate;
+                customerInDb.EndDate = customer.EndDate;
+                //Mapper.Map(customer, customerInDb); - Needs a Data Transfer Object - to limit properties that can be updated else its not secure to use this                
+            }            
+            //this wont add/update info to the DB just yet it just gets stored in memory to persist the changes we need to call this other method
+            _context.SaveChanges();            
+            
+            return Redirect("~/Customers/CustomersTable");
+        }
     }
 }
---
+++ 5StarWifi/5StarWifi/ViewModels/UpdateCustomerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _5StarWifi.Models;
+namespace _5StarWifi.ViewModels
+{
+    public class UpdateCustomerViewModel
+    {
+        public int SelectedMembershipTypeId { get; set; }
+        public IEnumerable<MembershipType> MembershipTypesList { get; set; }  
+        
+        public Customer Customer { get; set; }
+        public MembershipType MembershipType { get; set; }
+    }
+}
